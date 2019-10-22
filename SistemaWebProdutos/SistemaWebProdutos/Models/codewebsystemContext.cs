a614fbc58fd8d3bf1ab02d3027a60d2805f8e2b7
Leonardo Brancaglione <43422065+LBrancaglione@users.noreply.github.com>
2019-10-22 17:38:32 -0300

    SistemaWeb22_10

---
+++ SistemaWebProdutos/SistemaWebProdutos/Models/codewebsystemContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SistemaWebProdutos.Models
+{
+    public partial class codewebsystemContext : DbContext
+    {
+        public codewebsystemContext()
+        {
+        }
+
+        public codewebsystemContext(DbContextOptions<codewebsystemContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Produtos> Produtos { get; set; }
+
+      
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produtos>(entity =>
+            {
+                entity.HasKey(e => e.ProdCodigo);
+
+                entity.Property(e => e.ProdCodigo).HasColumnName("Prod_Codigo");
+
+                entity.Property(e => e.ProdNome)
+                    .IsRequired()
+                    .HasColumnName("Prod_Nome")
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.ProdPreco)
+                    .HasColumnName("Prod_Preco")
+                    .HasColumnType("decimal(14, 2)");
+
+                entity.Property(e => e.ProdQtde).HasColumnName("Prod_Qtde");
+            });
+        }
+    }
+}
